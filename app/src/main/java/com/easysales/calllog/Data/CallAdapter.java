ee00ff0198ec0758434b278c64cbcf4eb651b32d
lordpvi <lord.pvi@gmail.com>
2016-12-27 20:58:04 +0300

    дизайн списка вызовов

--- app/src/main/java/com/easysales/calllog/Data/CallAdapter.java
+++ app/src/main/java/com/easysales/calllog/Data/CallAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.easysales.calllog.Entities.Call;
 import com.easysales.calllog.R;
 import com.easysales.calllog.Utils.DataHelper;
+import com.easysales.calllog.Utils.SettingsHelper;
 
 import java.util.List;
 
@@ -42,36 +43,50 @@ public class CallAdapter extends BaseAdapter {
 
         ImageView imageView = (ImageView)view.findViewById(R.id.callGridItemImage);
         TextView titleView = (TextView)view.findViewById(R.id.callGridItemTextTitle);
+        TextView phoneNumberView = (TextView)view.findViewById(R.id.callGridItemPhoneNumber);
         TextView descriptionView = (TextView)view.findViewById(R.id.callGridItemTextDescription);
         ImageView imageSyncView = (ImageView) view.findViewById(R.id.callGridItemSyncImage);
 
         Call call = getItem(position);
 
+        String name = call.getName();
+        String number = "";
+        String description = "";
+
         switch (call.getType())
         {
             case Incoming:
                 imageView.setImageResource(R.drawable.incoming);
-                titleView.setText(call.getName() + " " + call.getNumberFrom());
-                descriptionView.setText(String.format("%1$s (%2$s)", DataHelper.ToDateTimeFormat(call.getBeginDate()), DataHelper.ToTimeFromat(call.getDuration() / 1000)));
+                number = call.getNumberFrom();
+                description = String.format("%1$s (%2$s)", DataHelper.ToDateTimeFormat(call.getBeginDate()), DataHelper.ToTimeFromat(call.getDuration() / 1000));
                 break;
             case Missed:
                 imageView.setImageResource(R.drawable.missed);
-                titleView.setText(call.getName() + " " + call.getNumberFrom());
-                descriptionView.setText(String.format("%1$s", DataHelper.ToDateTimeFormat(call.getBeginDate())));
+                number = call.getNumberFrom();
+                description = String.format("%1$s", DataHelper.ToDateTimeFormat(call.getBeginDate()));
                 break;
             case Outgoing:
                 imageView.setImageResource(R.drawable.outgoind);
-                titleView.setText(call.getName() + " " + call.getNumberTo());
-                descriptionView.setText(String.format("%1$s (%2$s)", DataHelper.ToDateTimeFormat(call.getBeginDate()), DataHelper.ToTimeFromat(call.getDuration() / 1000)));
+                number = call.getNumberTo();
+                description = String.format("%1$s (%2$s)", DataHelper.ToDateTimeFormat(call.getBeginDate()), DataHelper.ToTimeFromat(call.getDuration() / 1000));
                 break;
         }
+        if(name == null || name.length() == 0) {
+            name = number;
+        }
 
-        if(call.getIsSynchronized()) {
-            imageSyncView.setImageResource(R.drawable.ok);
+        titleView.setText(name);
+        phoneNumberView.setText(number);
+        descriptionView.setText(description);
+
+        if(SettingsHelper.getIsSynchronized()) {
+            if (call.getIsSynchronized()) {
+                imageSyncView.setImageResource(R.drawable.ok);
+            } else {
+                imageSyncView.setImageResource(R.drawable.cancel);
+            }
         }
-        else {
-            imageSyncView.setImageResource(R.drawable.cancel);
-        }
+
         return view;
     }
 
